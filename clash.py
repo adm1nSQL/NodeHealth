dea89b6e052ac80f4869b0653dbc15da923a72e8
Koi <58238284+adm1nSQL@users.noreply.github.com>
2024-01-23 07:56:45 +0800

    Update clash.py

--- clash.py
+++ clash.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 import time
 import signal
+import psutil
 from function import *
 
 
@@ -13,10 +14,14 @@ class Clash(object):
 
     def start_client(self):
         try:
+            # 检查端口是否被占用，如果被占用则终止对应进程
+            self._check_and_terminate_process(self, 7890)
+            self._check_and_terminate_process(self, 9090)
+
             client = f'{os.getcwd()}/mihomo'
             self._process = subprocess.Popen([f"{client}", '-f', './sub.yaml'], text=True)
             logger.info("启动mihomo客户端")
-            time.sleep(5)
+            time.sleep(30)
         except Exception as e:
             logger.error(str(e))
             sys.exit(1)
@@ -31,12 +36,25 @@ class Clash(object):
             # 等待进程退出
             self._process.wait()
             self._process = None
+            self._check_and_terminate_process(self, 7890)
+            self._check_and_terminate_process(self, 9090)
             logger.info("Client Stop!")
+
+    @staticmethod
+    def _check_and_terminate_process(self, port):
+        try:
+            # 查找占用指定端口的进程
+            for conn in psutil.net_connections(kind='inet'):
+                if conn.laddr.port == port:
+                    pid = conn.pid
+                    process = psutil.Process(pid)
+                    process.terminate()
+                    logger.info(f"端口{port}占用已解除，PID：{pid}")
+        except Exception as e:
+            logger.error(f"错误： {str(e)}")
 
 
 if __name__ == '__main__':
-    pass
-    # clash = Clash()
-    # clash.start_client()
-    # time.sleep(10)
-    # clash.stop_client()
+    clash = Clash()
+    clash.start_client()
+    clash.stop_client()
